44ee11d34675644e3916437a6f41d137cc3cfa4e
charan <charan@DESKTOP-OLCN4O6>
2025-02-24 19:37:34 +0530

    Added on class

---
+++ SampleMaven/src/test/java/github/AddClass.java
@@ -0,0 +1,12 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AddClass {
+	@Test
+	public void demo() {
+		Reporter.log("Add class Added", true);
+	}
+
+}
